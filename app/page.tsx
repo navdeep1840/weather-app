b21fb456b9d0461d1ad65b5088a4fa0d5474f502
Navdeep <navdeep3589@gmail.com>
2023-09-01 03:24:22 +0530

    bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { Card, Divider, Subtitle, Text } from "@tremor/react";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#394f68] to-[#183b7e] p-10 flex flex-col justify-center items-center   ">
-      <Card className="max-w-4xl mx-auto bg-white">
-        <Text className="text-5xl font-bold text-center mb-10">
+      <Card className="max-w-3xl mx-auto bg-white">
+        <Text className="text-4xl font-bold text-center mb-10">
           Weather App
         </Text>
         <Divider className="my-10" />
